c83443e367094bf8804cda385e0bf47ade3dea9f
Mikhail <s_m86@mail.ru>
2021-04-18 15:48:51 +0300

    Initial Commit

---
+++ randomizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  randomizer
+//
+//  Created by михаил сулим on 18.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
